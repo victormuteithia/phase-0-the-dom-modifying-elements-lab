fd280df0c287acc0b7fc0c888bcc947aa32ace7a
victormuteithia <muteithiavictor@gmail.com>
2024-07-24 03:15:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Victor is the champion"
